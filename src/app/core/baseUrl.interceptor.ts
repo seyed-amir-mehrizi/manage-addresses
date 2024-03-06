7a10a3fd9772e917410d6f9c81e1b851a2a25cd2
Amir Mehrizi <a.mehrizi@PIKAD.COM>
2024-03-06 18:47:06 +0330

    some refactor in interceptor

--- src/app/core/baseUrl.interceptor.ts
+++ src/app/core/baseUrl.interceptor.ts
@@ -19,35 +19,17 @@ export class BaseUrlInterceptor implements HttpInterceptor {
         request: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
-        console.log("request : ", request);
-        console.log("next : ", next);
-
-        // Add your logic to modify the request here
         let modifiedRequest;
         let token = localStorage.getItem('token')
 
         if (token) {
-            if (request.url.includes('/users')) {
+            if (request.url.includes('/users') || request.url.includes('/favorite-addresses')) {
 
                 modifiedRequest = request.clone({
                     setHeaders: {
                         'Authorization': `Bearer ${token}`,
                     },
                 });
-            }
-            else if (request.url.includes('/favorite-addresses')) {
-                const tokenParts = token.split('.');
-                // Parse the payload
-                const payload = JSON.parse(atob(tokenParts[1]));
-                payload.userId = 4;
-                tokenParts[1] = btoa(JSON.stringify(payload));
-                const newToken = tokenParts.join('.');
-                modifiedRequest = request.clone({
-                    setHeaders: {
-                        'Authorization': `Bearer ${newToken}`,
-                    },
-                });
-
             }
             else {
                 modifiedRequest = request;
@@ -57,15 +39,12 @@ export class BaseUrlInterceptor implements HttpInterceptor {
         }
         return next.handle(modifiedRequest).pipe(
             map((event: HttpEvent<any>) => {
-                // If the response is an HTTP success (e.g., 200 OK), you can handle it here
                 if (event instanceof HttpResponse) {
-                    // Add any specific handling for successful responses
                 }
                 return event;
             }),
             catchError((error: HttpErrorResponse) => {
                 console.log(error);
-
                 const expectedError = error.status >= 400 && error.status < 500;
                 if (expectedError) {
                     this.toastr.error(error.error)
